93a86277b1e3ba3ee5ecf0ddb08ac40de9ffe066
Andres Beck-Ruiz <andres_beck-ruiz@brown.edu>
2021-01-29 17:34:17 -0500

    test

--- src/main/java/edu/brown/cs/abeckrui/stars/Main.java
+++ src/main/java/edu/brown/cs/abeckrui/stars/Main.java
@@ -30,7 +30,7 @@ public final class Main {
    * The initial method called when execution begins.
    *
    * @param args
-   *          An array of command line arguments
+   *          An array of command line arguments...
    */
   public static void main(String[] args) {
     new Main(args).run();
